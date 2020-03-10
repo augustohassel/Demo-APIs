1eedd5532154723d72db78e2fd8e5660ee5005fc
Augusto Hassel <augusto.hassel@gmail.com>
2020-03-09 22:53:24 -0300

    Agregue html + pdf a la api

--- api/plumber.R
+++ api/plumber.R
@@ -8,34 +8,71 @@
 #
 
 library(plumber)
+library(rmarkdown)
 
-#' @get /hello
+#' @apiTitle Plumber Example API
+
+#* Listado de funciones disponibles
+#* @get /
 #' @html
 function(){
-  "<html><h1>hello world</h1></html>"
+  "<html>
+    <h1>Listado de Métodos Disponibles:</h1>
+    <ul>
+      <li><strong>/echo?msg='mensaje'</strong> - Repetir mensaje entre comillas</li>
+      <li><strong>/plot</strong> - Graficar histograma</li>
+      <li><strong>/sum?a=1&b=2</strong> - Sumar dos números</li>
+      <li><strong>/html?stock=BYMA.BA</strong> - Informe en HTML</li>
+      <li><strong>/pdf?stock=BYMA.BA</strong> - Informe en PDF</li>
+    </ul>
+  </html>"
 }
 
-#* @apiTitle Plumber Example API
-
-#* Echo back the input
-#* @param msg The message to echo
+#* Repetir el Mensaje entre comillas
 #* @get /echo
+#' @param msg:character Mensaje
 function(msg = "") {
-    list(msg = paste0("The message is: '", msg, "'"))
+    list(msg = paste0("El mensaje es: '", msg, "'"))
 }
 
-#* Plot a histogram
-#* @png
+#* Graficar histograma
 #* @get /plot
+#' @png
 function() {
     rand <- rnorm(100)
     hist(rand)
 }
 
-#* Return the sum of two numbers
-#* @param a The first number to add
-#* @param b The second number to add
-#* @post /sum
+#* Devuelve la suma de dos numeros
+#* @get /sum
+#' @param a:int El primer numero
+#' @param b:int El segundo numero
 function(a, b) {
     as.numeric(a) + as.numeric(b)
 }
+
+
+#* Informe en PDF
+#* @get /pdf
+#' @serializer contentType list(type="application/pdf; charset=utf-8")
+#' #' @param stock:character  Ticker obtenido desde Yahoo Finance
+function(res, stock="BTC-USD"){
+  temp <- tempfile(fileext = ".pdf")
+  rmarkdown::render("api/InformePDF.Rmd", 
+                    output_file = temp,
+                    output_format = NULL,
+                    params = list(stock = stock))
+  readBin(temp,'raw', n = file.info(temp)$size)
+}
+
+#* Informe en HTML
+#* @get /html
+#' @serializer contentType list(type="application/html; charset=utf-8")
+#' @param stock:character Ticker obtenido desde Yahoo Finance
+function(res, stock="BTC-USD"){
+  f <- rmarkdown::render("api/InformeHTML.Rmd", 
+                         output_format = NULL,
+                         params = list(stock = stock))
+  include_html(f, res)
+}
+
